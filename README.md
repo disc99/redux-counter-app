b6d10838f1f056da6bc577a02b005272ea2dcdc9
d.kanehira <disc99.g@gmail.com>
2017-04-27 22:38:00 +0900

    Create counter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # redux-counter-app
+
+@see https://egghead.io/courses/getting-started-with-redux
+@see http://qiita.com/insight3110/items/4d212ecef6992e8eaee5
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports={
-  entry: ["./src/app.js"],
+  entry: [__dirname + "/src/app.js"],
   output: {
     path: __dirname + "/dist",
     filename: "app.js"
@@ -9,7 +9,7 @@ module.exports={
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'babel-loader'
       }
     ]
   }
